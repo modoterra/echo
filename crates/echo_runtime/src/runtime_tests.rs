372445e608e2517888cef2cb18ab141ed05a8f74
Christoffer Hallas <git@christofferhallas.com>
2026-06-22 09:15:44 -0400

    refactor: split runtime stdlib tests

--- crates/echo_runtime/src/runtime_tests.rs
+++ crates/echo_runtime/src/runtime_tests.rs
@@ -1,7 +1,5 @@
 use super::*;
 use crate::collections::EchoArrayKey;
-use crate::value::{ECHO_VALUE_TCP_CONNECTION, ECHO_VALUE_TCP_LISTENER};
-use std::thread;
 
 fn test_string_value(bytes: &[u8]) -> EchoValue {
     EchoValue::string(Box::into_raw(Box::new(EchoString::new(bytes.to_vec()))))
@@ -19,52 +17,10 @@ mod encoding;
 mod filesystem;
 mod math;
 mod scalar;
+mod stdlib;
 mod string;
 mod task;
 mod value;
-
-#[test]
-fn std_net_abi_exchanges_loopback_bytes() {
-    let address = unsafe { echo_value_string(c"127.0.0.1:0".as_ptr().cast(), 11) };
-    let server = echo_std_net_listen(address);
-    assert_eq!(server.kind, ECHO_VALUE_TCP_LISTENER);
-
-    let listener = server.as_tcp_listener_ref().expect("listener");
-    let address = listener.local_addr().expect("local addr").to_string();
-
-    let client = thread::spawn(move || {
-        let address = unsafe { echo_value_string(address.as_ptr(), address.len()) };
-        let connection = echo_std_net_connect(address);
-        assert_eq!(connection.kind, ECHO_VALUE_TCP_CONNECTION);
-
-        let request = unsafe { echo_value_string(c"ping".as_ptr().cast(), 4) };
-        assert_eq!(echo_std_net_write(connection, request), EchoValue::int(4));
-        let response = echo_std_net_read(connection, EchoValue::int(4));
-        assert_eq!(response.string_bytes().expect("response"), b"pong");
-        assert_eq!(echo_std_net_close(connection), EchoValue::null());
-    });
-
-    let connection = echo_std_net_accept(server);
-    assert_eq!(connection.kind, ECHO_VALUE_TCP_CONNECTION);
-    let request = echo_std_net_read(connection, EchoValue::int(4));
-    assert_eq!(request.string_bytes().expect("request"), b"ping");
-    let response = unsafe { echo_value_string(c"pong".as_ptr().cast(), 4) };
-    assert_eq!(echo_std_net_write(connection, response), EchoValue::int(4));
-    assert_eq!(echo_std_net_close(connection), EchoValue::null());
-
-    client.join().expect("client");
-}
-
-#[test]
-fn std_http_response_text_formats_http_response() {
-    let body = unsafe { echo_value_string(c"hello".as_ptr().cast(), 5) };
-    let response = echo_std_http_response_text(body);
-
-    assert_eq!(
-        response.string_bytes().expect("response"),
-        b"HTTP/1.1 200 OK\r\ncontent-type: text/plain\r\ncontent-length: 5\r\nconnection: close\r\n\r\nhello"
-    );
-}
 
 #[test]
 fn explode_preserves_php_array_count_and_limit_behavior() {
