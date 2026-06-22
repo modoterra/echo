33310f422432c0e1b40f124a654f7d91cf2038e7
Christoffer Hallas <git@christofferhallas.com>
2026-06-22 10:24:49 -0400

    refactor: split runtime string comparison

--- crates/echo_runtime/src/string.rs
+++ crates/echo_runtime/src/string.rs
@@ -1,6 +1,12 @@
 use crate::encoding::{quoted_printable_decode_bytes, quoted_printable_encode_bytes};
 use crate::{EchoArray, EchoString, EchoValue, echo_runtime_string};
-use std::cmp::Ordering as CmpOrdering;
+
+mod compare;
+
+pub use compare::{
+    echo_php_strcasecmp, echo_php_strcmp, echo_php_strncasecmp, echo_php_strncmp,
+    echo_php_substr_compare,
+};
 
 const PHP_DEFAULT_TRIM_BYTES: &[u8] = b" \n\r\t\x0b\0";
 
@@ -939,67 +945,6 @@ pub extern "C" fn echo_php_substr_count(haystack: EchoValue, needle: EchoValue) 
     EchoValue::int(count)
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn echo_php_substr_compare(
-    haystack: EchoValue,
-    needle: EchoValue,
-    offset: EchoValue,
-    length: EchoValue,
-    case_insensitive: EchoValue,
-) -> EchoValue {
-    let Some(haystack) = haystack.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(needle) = needle.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(offset) = offset.int_value() else {
-        return EchoValue::error();
-    };
-    let Some(case_insensitive) = case_insensitive.bool_value() else {
-        return EchoValue::error();
-    };
-
-    let start = if offset < 0 {
-        let start = haystack.len() as i64 + offset;
-        if start < 0 {
-            return EchoValue::bool(false);
-        }
-        start as usize
-    } else {
-        offset as usize
-    };
-
-    if start > haystack.len() {
-        return EchoValue::bool(false);
-    }
-
-    let default_length = needle.len().max(haystack.len().saturating_sub(start));
-    let length = if length.is_null() {
-        default_length
-    } else {
-        let Some(length) = length.int_value() else {
-            return EchoValue::error();
-        };
-        let Ok(length) = usize::try_from(length) else {
-            return EchoValue::bool(false);
-        };
-        length
-    };
-
-    let haystack = &haystack[start..haystack.len().min(start + length)];
-    let needle = &needle[..needle.len().min(length)];
-    if case_insensitive {
-        EchoValue::int(case_insensitive_ascii_compare(haystack, needle))
-    } else {
-        EchoValue::int(match haystack.cmp(needle) {
-            CmpOrdering::Less => -1,
-            CmpOrdering::Equal => 0,
-            CmpOrdering::Greater => 1,
-        })
-    }
-}
-
 fn find_bytes_ascii_case_insensitive(haystack: &[u8], needle: &[u8]) -> Option<usize> {
     if needle.is_empty() {
         return Some(0);
@@ -1016,102 +961,4 @@ fn bytes_eq_ascii_case_insensitive(left: &[u8], right: &[u8]) -> bool {
             .iter()
             .zip(right)
             .all(|(left, right)| left.eq_ignore_ascii_case(right))
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn echo_php_strcmp(left: EchoValue, right: EchoValue) -> EchoValue {
-    let Some(left) = left.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(right) = right.string_bytes() else {
-        return EchoValue::error();
-    };
-
-    EchoValue::int(match left.cmp(&right) {
-        CmpOrdering::Less => -1,
-        CmpOrdering::Equal => 0,
-        CmpOrdering::Greater => 1,
-    })
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn echo_php_strcasecmp(left: EchoValue, right: EchoValue) -> EchoValue {
-    let Some(left) = left.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(right) = right.string_bytes() else {
-        return EchoValue::error();
-    };
-
-    EchoValue::int(case_insensitive_ascii_compare(&left, &right))
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn echo_php_strncmp(
-    left: EchoValue,
-    right: EchoValue,
-    length: EchoValue,
-) -> EchoValue {
-    let Some(left) = left.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(right) = right.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(length) = length.int_value() else {
-        return EchoValue::error();
-    };
-    let Ok(length) = usize::try_from(length) else {
-        return EchoValue::error();
-    };
-
-    EchoValue::int(
-        match left[..left.len().min(length)].cmp(&right[..right.len().min(length)]) {
-            CmpOrdering::Less => -1,
-            CmpOrdering::Equal => 0,
-            CmpOrdering::Greater => 1,
-        },
-    )
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn echo_php_strncasecmp(
-    left: EchoValue,
-    right: EchoValue,
-    length: EchoValue,
-) -> EchoValue {
-    let Some(left) = left.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(right) = right.string_bytes() else {
-        return EchoValue::error();
-    };
-    let Some(length) = length.int_value() else {
-        return EchoValue::error();
-    };
-    let Ok(length) = usize::try_from(length) else {
-        return EchoValue::error();
-    };
-
-    EchoValue::int(case_insensitive_ascii_compare(
-        &left[..left.len().min(length)],
-        &right[..right.len().min(length)],
-    ))
-}
-
-fn case_insensitive_ascii_compare(left: &[u8], right: &[u8]) -> i64 {
-    for (left, right) in left.iter().zip(right) {
-        let left = left.to_ascii_lowercase();
-        let right = right.to_ascii_lowercase();
-
-        if left != right {
-            return left as i64 - right as i64;
-        }
-    }
-
-    match left.len().cmp(&right.len()) {
-        CmpOrdering::Less => -1,
-        CmpOrdering::Equal => 0,
-        CmpOrdering::Greater => 1,
-    }
 }
